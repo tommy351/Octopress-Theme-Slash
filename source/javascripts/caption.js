84695ac13a34ade6a7752b127ff93ca8a3ffddf9
SkyArrow <tommy351@gmail.com>
2012-01-25 01:18:01 +0800

    include fancybox

--- source/javascripts/caption.js
+++ source/javascripts/caption.js
@@ -1,9 +1,15 @@
 (function($){
-	$('.entry img').each(function(){
-		var alt = $(this).attr('alt');
+	$('.entry').each(function(i){
+		var _i = i;
+		$(this).find('img').each(function(){
+			var alt = $(this).attr('alt');
 
-		if (alt != ''){
-			$(this).after('<span class="caption">'+alt+'</span>');
-		}
+			if (alt != ''){
+				$(this).after('<span class="caption">'+alt+'</span>').wrap('<a href="'+$(this).attr('src')+'" class="fancybox" title="'+alt+'" rel="gallery'+_i+'" />');
+			} else {
+				$(this).wrap('<a href="'+$(this).attr('src')+'" class="fancybox" rel="gallery'+_i+'" />');
+			}
+		});
 	});
+	$('.fancybox').fancybox();
 })(jQuery);
