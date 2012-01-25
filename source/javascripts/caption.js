90de8379c142039bb1f9c78a78b3c8b308a24727
SkyArrow <tommy351@gmail.com>
2012-01-25 13:36:17 +0800

    fix caption is undefined when alt is not defined

--- source/javascripts/caption.js
+++ source/javascripts/caption.js
@@ -4,10 +4,10 @@
 		$(this).find('img').each(function(){
 			var alt = $(this).attr('alt');
 
-			if (alt != ''){
-				$(this).after('<span class="caption">'+alt+'</span>').wrap('<a href="'+$(this).attr('src')+'" class="fancybox" title="'+alt+'" rel="gallery'+_i+'" />');
-			} else {
+			if (alt == '' || typeof alt == 'undefined'){
 				$(this).wrap('<a href="'+$(this).attr('src')+'" class="fancybox" rel="gallery'+_i+'" />');
+			} else {
+				$(this).after('<span class="caption">'+alt+'</span>').wrap('<a href="'+$(this).attr('src')+'" class="fancybox" title="'+alt+'" rel="gallery'+_i+'" />');
 			}
 		});
 	});
